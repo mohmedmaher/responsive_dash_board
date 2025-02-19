89c74e1617510960ff8086e44fb1981f84f8db61
mohammedmaher <mohammedmaher3333333@gmail.com>
2025-02-19 08:12:02 +0200

    add images and font and style and adaptive laypot

---
+++ lib/views/dash_board_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/views/widgets/desktop_dash_board_layout.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => SizedBox(),
+        tabletLayout: (context) => SizedBox(),
+        desktopLayout: (context) => DesktopDashBoardLayout(),
+      ),
+    );
+  }
+}
